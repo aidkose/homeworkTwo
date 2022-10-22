d9dcdcf34cf0c697ab8ffacde99885f7273e99c7
Aiden Kosegi <aidkose@iu.edu>
2022-10-21 20:24:04 -0400

    Just got nav working properly
    Assignment has been rough. Just got nav working. Moving on to filling pages with content.

--- app/app.js
+++ app/app.js
@@ -3,12 +3,35 @@ function initListeners() {
 		$('.bars').toggleClass('active');
 		$('.links').toggleClass('active');
 	});
-	$('.links a').click(function (e) {
+	$('.links').click(function (e) {
 		$('.bars').toggleClass('active');
 		$('.links').toggleClass('active');
 	});
 }
 
+function changePage() {
+	let hash = window.location.hash;
+	let hashID = hash.replace('#', '');
+	// console.log(hash + ' ' + hashID);
+	if (hashID != '') {
+		$.get(`pages/${hashID}/${hashID}.html`, function (data) {
+			// console.log('data ' + data);
+			$('#app').html(data);
+		});
+	} else {
+		$.get(`pages/home/home.html`, function (data) {
+			// console.log('data' + data);
+			$('#app').html(data);
+		});
+	}
+}
+
+function initHashListener() {
+	$(window).on('hashchange', changePage);
+	changePage();
+}
+
 $(document).ready(function () {
 	initListeners();
+	initHashListener();
 });
